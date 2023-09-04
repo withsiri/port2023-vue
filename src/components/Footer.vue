8c9008461a3b5304a4bc8fb4e2e10165eaac5330
m2 <withsiri91@gmail.com>
2023-09-04 22:53:35 +0900

    푸터 copyright 수정

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -29,7 +29,7 @@ import { footerText } from "../constants";
             <!-- //footer_info -->
             <div class="footer_bottom">
                 &copy;2023 withsiri91<br />
-                이 사이트는 비트를 이용하여 제작하였습니다.
+                이 사이트는 뷰를 이용하여 제작하였습니다.
             </div>
             <!-- //footer_bottom -->
         </div>
